c1b7dfb7f1ab9b9b38a50c25ab9b98400fc66b4b
ANTONISMICHAILIDIS <ANTONIIMICH@gmail.com>
2025-02-04 19:50:09 +0200

    Update streamline2_app.py

--- streamline2_app.py
+++ streamline2_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
-import random
+
 
 # Function to generate nurse shift schedule
 def generate_schedule(nurses, unavailable_days, shift_preferences, month, year):
